ad9adfec3368a25ce0ff3d404aeb27035c498448
DGMMX <dgmmxs2@outlook.com>
2025-02-19 22:25:38 -0300

    montes feitos

--- src/main/java/com/example/Monte.java
+++ src/main/java/com/example/Monte.java
@@ -7,6 +7,20 @@ public class Monte {
 
     private List<Carta> cartas = new ArrayList<>();
 
-    public Monte
+    public Monte(){
+        for(int numero = 1; numero < 13; numero++){
+           for (Naipe naipe : Naipe.values()){
+            cartas.add(new Carta(numero, naipe) );
+           }
+        }
+    }
+
+    public void embaralhar(){
+        collections.shuffle(cartas);
+    }
+
+    public Cartas virar(){
+        return cartas.remove(0);
+    }
 
 }
